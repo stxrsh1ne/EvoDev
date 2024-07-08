f8c1b30f255c79d734694144b4b72f16c339703c
stxrsh1ne <iaroslav.iliushkin@gmail.com>
2024-07-08 22:01:37 +0300

    #task-1.2 fix: fix problems

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'EvoDev';
+export class AppComponent implements OnInit {
+  @Input() title!: string;
+  @Output() titleChange = new EventEmitter<string>();
+
+  ngOnInit() {
+    this.title = "swag";
+    this.titleChange.emit(this.title);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello/hello.component';
 import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent,
     CounterComponent
   ],
   imports: [
