59bd3f3849ff9da0b6a19923cdf7f7aa49eaabaa
stxrsh1ne <iaroslav.iliushkin@gmail.com>
2024-07-16 01:09:26 +0300

    #task-1.8 feat: start work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'EvoDev';
+export class AppComponent implements OnInit {
+  counter = 0;
+  level = 1;
+  lastClickTime: number = 0;
+  clickThreshold: number = 100;
+
+  ngOnInit() {
+  }
+
+  changeCount(type: boolean) {
+    const currentTime = Date.now();
+    if (currentTime - this.lastClickTime < this.clickThreshold) {
+      this.notiflix_alert('warning');
+    } else {
+      if (type) {
+        this.counter++;
+        if (this.counter >= 100) {
+          this.counter = 0;
+          this.level++;
+          this.notiflix_alert('level-up');
+        }
+      } else {
+        this.counter--;
+        if (this.counter < 0) {
+          this.counter = 0;
+          this.level = 1;
+        }
+      }
+      this.lastClickTime = currentTime;
+    }
+  }
+
+  notiflix_alert(type: string) {
+    switch (type) {
+      case 'success':
+        Notify.success('SUCCESS CLICK');
+        break;
+      case 'error':
+        Notify.failure('PROGRESS RESET');
+        this.counter = 0;
+        this.level = 1;
+        break;
+      case 'warning':
+        Notify.warning('CLICK SLOWLY');
+        break;
+      case 'level-up':
+        Notify.success(`LEVEL UP! Current Level: ${this.level}`);
+        break;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ProgressbarModule} from 'ngx-bootstrap/progressbar';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ProgressbarModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
