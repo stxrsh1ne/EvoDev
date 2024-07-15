64655bdf2878994f691354c8b8513397739b044e
stxrsh1ne <iaroslav.iliushkin@gmail.com>
2024-07-15 21:22:49 +0300

    #task-1.7 fix: fix bags

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InlineComponent } from './inline/inline.component';
-import { OpenGraphComponent } from './open-graph/open-graph.component';
 import { Meta } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
     AppComponent,
     InlineComponent,
-    OpenGraphComponent
   ],
   imports: [
     BrowserModule,
