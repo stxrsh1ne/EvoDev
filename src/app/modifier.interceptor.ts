45fdebf71845f355ea8412eaf432939c4406fce6
stxrsh1ne <iaroslav.iliushkin@gmail.com>
2024-07-30 08:47:03 +0300

    #task-2.5 fix: fix error

--- src/app/modifier.interceptor.ts
+++ src/app/modifier.interceptor.ts
@@ -13,8 +13,8 @@ export class ModifierInterceptor implements HttpInterceptor {
   constructor() {
   }
 
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    request = request.clone({url: `${request.url}?id=1`});
-    return next.handle(request);
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    const modifiedReq = request.clone({url: `${request.url}/1`});
+    return next.handle(modifiedReq);
   }
 }
